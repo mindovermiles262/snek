0b9ac654f3fda7b8472a4b99858fd64f0b6479d8
Andy Duss <mindovermiles262@gmail.com>
2017-11-07 11:12:46 -0800

    add grid

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 let container = document.getElementById("container")
 let pixel = document.createElement("div")
 
+let grid = {
+  row0: ["1", "0", "0", "0", "0"],
+  row1: ["0", "0", "0", "0", "0"],
+  row2: ["0", "0", "1", "0", "0"],
+  row3: ["0", "0", "0", "1", "0"],
+  row4: ["0", "0", "0", "0", "1"],
+}
+// Object.keys(grid).length => 5
+
 let putGrid = function(x, y) {
   for (i=0; i < x; i++) {
-    for (j=0; j < y; j++) {
-      container.appendChild(document.createElement("div")).className = "pixel"
-    }
+    let row = "row" + i
+    grid[row].forEach(function(element, index) {
+      if (grid[row][index] === "0") {
+        let pixel = container.appendChild(document.createElement("div"))
+        pixel.className = "pixel"
+        pixel.style.backgroundColor = "goldenrod"
+      } else {
+        container.appendChild(document.createElement("div")).className = "pixel"
+      }
+    })
   }
 }
 
